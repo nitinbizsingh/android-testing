bd99c5c3dee27aa9b7193ae2dd92bd570ad5ce3d
nitin <nitin.singh@foundermark.com>
2021-08-12 18:01:06 +0530

    -Minor changes

--- HydrateMe/app/src/main/java/com/example/hydrateme/CreateEntryActivity.kt
+++ HydrateMe/app/src/main/java/com/example/hydrateme/CreateEntryActivity.kt
@@ -1,11 +1,11 @@
 package com.example.hydrateme
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
 
 private const val TAG = "CreateEntryActivity"
 
@@ -19,7 +19,11 @@ class CreateEntryActivity : AppCompatActivity() {
         val addButton: View = findViewById(R.id.add_entry_button)
         val entryEditText: EditText = findViewById(R.id.intake_edit_text)
         addButton.setOnClickListener { view ->
-            Log.d(TAG, entryEditText.text.toString())
+            Snackbar.make(view, resources.getText(R.string.saved), Snackbar.LENGTH_LONG)
+                .show()
+
+            val intakeInMl = entryEditText.text.toString().toInt()
+            Log.d(TAG, intakeInMl.toString() + " ml")
         }
     }
 }
